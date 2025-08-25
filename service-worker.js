c87a862f03d2e982e6141536472ffab1b7b9cfce
teedublu <160520432+teedublu@users.noreply.github.com>
2025-08-25 08:15:29 +0100

    stuff

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE = 'cough-tracker-v1';
+const ASSETS = [
+  'cough-tracker-standalone.html',
+  'manifest.webmanifest',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.destination === 'document') {
+    e.respondWith(
+      fetch(req).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return r;
+      }).catch(() => caches.match(req))
+    );
+    return;
+  }
+  e.respondWith(caches.match(req).then(m => m || fetch(req)));
+});
